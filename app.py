9f8b351934a688fe1a240b85e43d60c825f589de
Moses Aondofa Tyav <tyav2greenz@gmail.com>
2025-02-08 19:52:07 +0100

    Change environment variable for Flask port configuration

--- app.py
+++ app.py
@@ -187,5 +187,5 @@ def logout():
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.getenv('FLASK_RUN_PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(debug=True, port=port)
