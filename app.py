39b4634bf4021aa0205aac62cc60df11acf0963e
Moses Aondofa Tyav <tyav2greenz@gmail.com>
2025-02-08 20:13:04 +0100

    Change Flask app to run on localhost instead of all interfaces

--- app.py
+++ app.py
@@ -187,5 +187,5 @@ def logout():
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
-    app.run(debug=True, port=port, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, port=port, host='localhost')
